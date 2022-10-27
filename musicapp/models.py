1a31eb5f327d2c88d7c83159544b0198cbc08a63
Daniel Adesina <danieladesina70@gmail.com>
2022-10-28 00:39:31 +0100

    gitignore

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,22 @@
+from datetime import datetime
 from django.db import models
 
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=40)
+    Last_name = models.CharField(max_length=40)
+    Age = models.IntegerField()
+
+class Song(models.Model):
+    Artiste = models.ForeignKey('Artiste', on_delete=models.CASCADE)
+    Title =models.TextField(max_length=400)
+    Date_released = models.DateTimeField(default=datetime.today)
+    Likes = models.IntegerField()
+    artiste_code = models.IntegerField()
+
+    
+class Lyrics(models.Model):
+   Song = models.ForeignKey(Song, on_delete=models.CASCADE)
+   Content = models.TextField(max_length=400)
+   song_code = models.IntegerField()
